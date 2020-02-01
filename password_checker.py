eb1ac1cafe387b99b31dda760836078e670fb7c5
S.L. Ting <drikting@gmail.com>
2020-02-02 03:33:14 +0800

    password checker

--- password_checker.py
+++ password_checker.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -10,17 +11,30 @@ def request_api_data(query_char):
     return response
 
 
-def read_response(response):
-    print(response.text)
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if hash_to_check == h:
+            return count
+    return 0
 
 
 def pawned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5, tail = sha1password[:5], sha1password[5:]
     res = request_api_data(first5)
-    read_response(res)
+    return get_password_leaks_count(res, tail)
 
 
-# hashed = pawned_api_check("password123")
-# print(request_api_data(hashed))
-pawned_api_check("password124")
+def main(args):
+    for check in args:
+        count = pawned_api_check(check)
+        if count:
+            print(f'Password {check} has been leaked or hacked {count} number of times. Time to change?')
+        else:
+            print('{check} was NOT leaked. Good job, carry on!')
+    return 'Program finished!'
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
